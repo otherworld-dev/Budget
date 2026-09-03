5e800f78114e64097b5f80803f270d7c155f55a7
Adam Morgan <adam@otherworld.dev>
2026-09-03 23:53:59 +0100

    chore(l10n): compile translations after the Weblate sync (adds Dutch)

---
+++ budget/l10n/nl.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "budget",
+    {
+    "Failed to retrieve accounts" : "Ophalen van rekeningen mislukt",
+    "Account not found" : "Rekening niet gevonden",
+    "Account" : "Rekening",
+    "Invalid account type: %1$s" : "Ongeldig rekeningtype: %1$s",
+    "Invalid currency: %1$s" : "Ongeldige valuta:%1$s",
+    "Institution" : "Institutie",
+    "Account holder name" : "Naam rekeninghouder",
+    "Statement day must be between 1 and 31" : "Afschriftdatum moet tussen 1 en 31 zijn"
+},
+"nplurals=2; plural=n != 1;");
